159bf63de779d65cc9515b44dda22c12aad05c32
alex <avaulin@rtk-element.ru>
2023-11-18 10:04:05 +0700

    Повысил версию спринга

---
+++ src/main/java/com/ngu/restclienttemplate/controller/AuthController.java
@@ -0,0 +1,77 @@
+package com.ngu.restclienttemplate.controller;
+
+import com.ngu.restclienttemplate.auth.UserService;
+import com.ngu.restclienttemplate.dto.UserDto;
+import com.ngu.restclienttemplate.entity.User;
+import com.ngu.restclienttemplate.utils.CheckAuth;
+import jakarta.jws.WebParam;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collection;
+import java.util.List;
+
+@Controller
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("index")
+    public String home(Model model) {
+
+        if (CheckAuth.isUser()) {
+            model.addAttribute("isUser", true);
+        }
+        return "main";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+
+
+    @GetMapping("register")
+    public String showRegistrationForm(Model model) {
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+
+    @PostMapping("/register/save")
+    public String registration(@ModelAttribute("user") UserDto user,
+                               BindingResult result,
+                               Model model) {
+        User existing = userService.findByEmail(user.getEmail());
+        if (existing != null) {
+            result.rejectValue("email", null, "There is already an account registered with that email");
+        }
+        if (result.hasErrors()) {
+            model.addAttribute("user", user);
+            return "register";
+        }
+        userService.saveUser(user);
+        return "redirect:/register?success";
+    }
+
+    @GetMapping("/users")
+    public String listRegisteredUsers(Model model) {
+        List<UserDto> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "users";
+    }
+}
--- src/main/java/com/ngu/restclienttemplate/controller/RestController.java
+++ src/main/java/com/ngu/restclienttemplate/controller/RestController.java
@@ -1,38 +1,18 @@
 package com.ngu.restclienttemplate.controller;
 
-import com.ngu.restclienttemplate.service.GroupService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class RestController {
 
-    @Autowired
-    private GroupService groupService;
-
-    @GetMapping(value = "/")
-    public String redirect() {
-        return "redirect:/main";
-    }
-
-    @GetMapping(value = "/main")
+    @GetMapping(value = "/test")
     public String welcome(Model model) {
         model.addAttribute("data", "Привет");
-        return "main";
-    }
-
-    @GetMapping(value = "list/groups")
-    private String getListGroup(Model model) {
-        model.addAttribute("groups", groupService.getGroups());
-        return "group-list";
-    }
-
-    @GetMapping(value = "list/students")
-    private String getListStudent(Model model) {
-        model.addAttribute("students", groupService.generateMockStudents(null));
-        return "student-list";
+        return "test";
     }
 
 
