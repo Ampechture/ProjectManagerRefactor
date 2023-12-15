a527bbcb7d485687c2d4c05754a6a693d8c792a6
Ampechture <55424626+Ampechture@users.noreply.github.com>
2023-12-15 20:51:47 +0700

    Delete src directory

--- src/main/java/com/ngu/restclienttemplate/repositories/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ngu.restclienttemplate.repositories;
-
-import com.ngu.restclienttemplate.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-
-}
